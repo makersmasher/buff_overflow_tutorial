6cf1571ac89fa78a7e06e06584798d8299ba1021
anthony <anthony@desktop.local>
2021-01-21 12:05:47 -0600

    initial commit

---
+++ username.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+	char destination[7];
+	printf("%s\n", argv[1]);
+	strcpy(destination, argv[1]);
+
+			return 0;
+	
+}
